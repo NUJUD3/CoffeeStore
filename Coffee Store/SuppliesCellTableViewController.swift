2bdf02640df6f6a853e456568537f7ee0696e2e5
NUJUD3 <nojoudmohaimmed@hotmail.com>
2021-11-03 12:12:06 +0300

    fix issues add app icon

--- Coffee Store/SuppliesCellTableViewController.swift
+++ Coffee Store/SuppliesCellTableViewController.swift
@@ -126,8 +126,9 @@ class SuppliesCellTableViewController: UITableViewController, CartDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-        
+        if (segue.identifier == "acSegue"){
         let cartVC = segue.destination as! CartTableViewController
             cartVC.cartItems = cartItems
+        }
     }
 }
